662a9d55f6cd3346a57758e232c1f3dec1ed6b88
Dmitrii Strelnikov <46859729+jordan1ce@users.noreply.github.com>
2019-01-27 18:50:56 +0300

    Урок 1, Ex1
    Проверка конфигурации

---
+++ javaAppiumAutomation/src/FirstTest.java
@@ -0,0 +1,37 @@
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.URL;
+
+public class FirstTest {
+
+    private AppiumDriver driver;
+
+    @Before
+    public void setUp() throws Exception {
+
+        DesiredCapabilities capabilitis = new DesiredCapabilities();
+
+        capabilitis.setCapability("platformName", "Android");
+        capabilitis.setCapability("deviceName", "AndroidTestDevice");
+        capabilitis.setCapability("platformVersion", "6.0");
+        capabilitis.setCapability("automationName", "Appium");
+        capabilitis.setCapability("appPackage", "org.wikipedia");
+        capabilitis.setCapability("appActivity", ".main.MainActivity");
+        capabilitis.setCapability("app", "C:\\Users\\Dmitrii\\Desktop\\Моя\\javaAppiumAutomation\\apks\\org.wikipedia.apk");
+        driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilitis);
+    }
+    @After
+    public void tearDown() {
+        driver.quit();
+    }
+    @Test
+    public  void fierstTest() {
+        System.out.println("First test Run");
+    }
+}
+
